2896cf406ea91cfa3481131150d38c74fd88effb
Nicat.dcw <59221034+Nicat-dcw@users.noreply.github.com>
2020-08-04 00:49:04 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+/* Starting */ /* Nicat.dcw */
+/* MIT LICENSE */ /* Çalmak Yasaktır */
+
+
+const discord = require('discord.js');
+const db = require('quick.db');
+client.login(TOKEN);
+client.prefix = PREFIX;
